b9b00a32a0a8a37ce4a885292736cd029e8679db
Sasri Samarawickrama <90295@maharaja.lk>
2020-08-11 16:07:51 +0530

    first commit

---
+++ resources/views/employee.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+    <body>
+        <form >
+           User ID: <input type = "text" name = "user_id" />
+           <br>
+           Name: <input type = "text" name = "name" />
+           <br>
+           Mobile: <input type = "text" name = "tel" />
+           <br>
+           Address: <input type = "text" name = "address" />
+           <br>
+           NIC: <input type = "text" name = "nic" />
+           <br>
+           <input type="submit" value="Submit">
+        </form>
+     </body>
+</body>
+</html>
